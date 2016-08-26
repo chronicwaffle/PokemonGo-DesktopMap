1cc9fdc756e4ea5ecffc042fbc49e922b4e6868a
chronicwaffle <jdunk576@gmail.com>
2016-08-26 06:08:23 -0500

    Revert "Update from original"

--- package_python.sh
+++ package_python.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-source ve/bin/activate
-pip install -r app/map/requirements.txt --target ./packages
+source map/ve/bin/activate
+cd map
+pip install -r requirements.txt --target ./packages
 touch ./packages/google/__init__.py
 if [ -d packages ]; then
   cd packages
   find . -name "*.pyc" -delete
-  find . -name "*.so" -delete
-  find . -name "*.dist-info" | xargs rm -rf
   find . -name "*.egg-info" | xargs rm -rf
-  find . -name "Cryptodome" | xargs rm -rf
-  find . -name "pyproj" | xargs rm -rf
-  find . -name "xxhash" | xargs rm -rf
-  rm -fr "babel/locale-data"
   zip -9mrv packages.zip .
-  rm -fr ../app/packages.zip
-  mv packages.zip ../app/
+  mv packages.zip ..
   cd ..
   rm -rf packages
 fi
