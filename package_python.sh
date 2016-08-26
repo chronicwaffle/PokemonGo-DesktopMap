5698bae91f91043f44d5ac07b5ddf5119ac28410
chronicwaffle <jdunk576@gmail.com>
2016-08-26 05:46:15 -0500

    Update from original (#1)
    * Updated README
    
    * Refactor python project
    
    * Remove google login code
    
    * More packaging
    
    * Updating app to use new run_map, gruntfile
    
    * Windows installer updates
    
    * Remove package.json from map app, causing problems
    
    * Reverting to old Electron version

--- package_python.sh
+++ package_python.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-source map/ve/bin/activate
-cd map
-pip install -r requirements.txt --target ./packages
+source ve/bin/activate
+pip install -r app/map/requirements.txt --target ./packages
 touch ./packages/google/__init__.py
 if [ -d packages ]; then
   cd packages
   find . -name "*.pyc" -delete
+  find . -name "*.so" -delete
+  find . -name "*.dist-info" | xargs rm -rf
   find . -name "*.egg-info" | xargs rm -rf
+  find . -name "Cryptodome" | xargs rm -rf
+  find . -name "pyproj" | xargs rm -rf
+  find . -name "xxhash" | xargs rm -rf
+  rm -fr "babel/locale-data"
   zip -9mrv packages.zip .
-  mv packages.zip ..
+  rm -fr ../app/packages.zip
+  mv packages.zip ../app/
   cd ..
   rm -rf packages
 fi
